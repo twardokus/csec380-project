444a88e12f1344a2c2298bb0e9bacb9c21528ee1
Andrew Afonso <andrewbluepiano@hotmail.com>
2019-09-18 18:12:32 -0400

    initial site commit

---
+++ site/public_html/proc/footer.php
@@ -0,0 +1,6 @@
+<!-- Copyright (c) 2019 Andrew Afonso, just leave my name in comments if you reuse -->
+</div>
+<footer>
+	<img class="scorelogo" src="/assets/img/transparent_logo.png">
+</footer>
+</div>
---
+++ site/public_html/proc/head.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="css/style.css" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="icon" href="/assets/icons/favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="/assets/icons/favicon.ico" type="image/x-icon"/>
+<meta name="viewport" content="initial-scale=1">
