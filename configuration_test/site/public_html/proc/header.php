8d68a79cba24023d98e71e07b4b92fbfecc403e9
Geoff Twardokus <gdt5762@rit.edu>
2019-10-26 15:59:56 -0400

    Restructured repo to put Activity 2 test files together out of the way

---
+++ configuration_test/site/public_html/proc/header.php
@@ -0,0 +1,19 @@
+<div id="smallWrap">
+<div id="smallWrapContent">
+<header>
+<nav>
+	<?php echo $headtext; ?>
+ 	<section id="HamburgToggle">
+		<input type="checkbox"/>
+		<span></span>
+		<span></span>
+		<span></span>
+		<ul id="Menu">
+		  <a href="index.php"><li>Home</li></a>
+		  <a href="videos.php"><li>Videos</li></a>
+		  <a href="login.php"><li>login</li></a>
+		  <a class="Logout" href="logout.php"><li>Logout</li></a>
+		</ul>
+  </section>
+</nav>
+</header>
