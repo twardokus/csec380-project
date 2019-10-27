f3813d570f6902642e60fc8eeb1e70e7644fbdd5
Andrew Afonso <andrewbluepiano@hotmail.com>
2019-10-27 15:12:41 -0400

    updated credential variable names

--- html/loginvalidate.php
+++ html/loginvalidate.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
 	$config = parse_ini_file($_SERVER["DOCUMENT_ROOT"]."/tempvuln/config.ini"); 	
-	mysql_connect($config['serverread'], $config['usernameread'], $config['passwordread']) or DIE('Unable to connect to NAS, check if SQL server is enabled');
-	mysql_select_db($config['dbnameread']) or DIE('Database is not available!');
+	mysql_connect($config['server'], $config['username'], $config['password']) or DIE('Unable to connect to NAS, check if SQL server is enabled');
+	mysql_select_db($config['dbname']) or DIE('Database is not available!');
 	// query DB for username and password entery given by input. Note output from MD5 function passed as password:
 	$login = mysql_query("SELECT * FROM users WHERE (email = '" . mysql_real_escape_string($_POST['username']) . "') and (password = '" . mysql_real_escape_string(md5($_POST['password'])) . "')");
 	// Check username and password match
