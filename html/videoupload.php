8c6c2600fed616481ea9a8b0251b06b4fdf92bda
Andrew Afonso <ara1494@rit.edu>
2019-11-07 22:18:22 -0500

    Create videoupload.php

---
+++ html/videoupload.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['username'])) {
+		header('Location: login.php');
+	}
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Video Uploader</title>
+		<?php include("proc/head.php"); ?>
+	</head>
+	<body>
+	<?php $headtext = "<h1>Video Uploader</h1>"; include("proc/header.php"); ?>
+	
+	<div class="vcenter text">
+		<div class="vcenterholder center">
+			<h1>Enter details of video</h1>
+			<form action="proc/uploader.php" method="post" enctype="multipart/form-data">
+				<p>Title: <input id="softfield" type="text" name="vidtitle" placeholder="catz" required></p>
+				<p>Description (optional): <input id="softfield" type="textarea" name="viddescription"></p>
+				Select video file (mp4) to upload:
+				<input type="file" name="upfile" id="upfile">
+				<input type="submit" value="Upload File" name="submit">
+			</form>
+		</div>
+	</div>
+	<footer>
+</footer>
+	</body>
+</html>
