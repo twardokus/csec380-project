877f434cca9cf347b3ac70ab59b65c470acefe7a
Andrew Afonso <andrewbluepiano@hotmail.com>
2019-11-16 18:26:46 -0500

    so amazing now

--- html/videos.php
+++ html/videos.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
-
 	if (!isset($_SESSION['username'])) {
 		header('Location: login.php');
 	}
+    require_once('proc/writerObj.php');
 ?>
 <html>
 <head>
@@ -12,7 +12,28 @@
 </head>
 <body>
 	<?php $headtext = "<h1>Videos</h1>"; include("proc/header.php"); ?>
-	<div class="vcenter text">
+    <div class="vcenter text">
+    <div class="vcenterholder center">
+        <?php
+            $getvideos = "SELECT * FROM videos;";
+            $returnedvids = $sqlconn->query($getvideos);
+//            print_r($returnedvids);
+            $videos = array();
+            while ($row = mysqli_fetch_assoc($returnedvids)) {
+                $videos[] = $row;
+            }
+//            print_r($videos);
+            foreach($videos as $value){
+                $getemail = "SELECT email FROM users WHERE user_id='".$value['ownerid']."';";
+                $emailreturned = $sqlconn->query($getemail);
+                $theuploader = mysqli_fetch_assoc($emailreturned);
+                $uploaderEmail = $theuploader['email'];
+                echo "<article class=\"video\"><video width=\"320\" height=\"240\" controls><source src=\"/videos/". $value['ownerid'] ."/". $value['titlehash']."\" type=\"video/mp4\">Your browser does not support the video tag.</video><p>Uploaded by: ".$uploaderEmail."</p></article>";
+            }
+            
+        ?>
+ 
+	<!--<div class="vcenter text">
 		<div class="vcenterholder center">
 			<h1>All</h1>
 			<article class="video">
@@ -28,7 +49,7 @@
 					Your browser does not support the video tag.
 				</video>
 				<p>Uploaded by: USERNAME</p>
-			</article>
+			</article>-->
 		</div>
 	</div>
 	</div>
