fe07714b20c8e12ce61895430ebb8ba2da5b7633
Andrew Afonso <andrewbluepiano@hotmail.com>
2019-11-16 20:01:26 -0500

    removed debugging stuff

--- html/proc/deletevideo.php
+++ html/proc/deletevideo.php
@@ -6,25 +6,18 @@ if (!isset($_SESSION['username'])) {
 }
 $vidtitle = filter_input(INPUT_POST, 'videoHash');
 $vidtitle = substr($vidtitle, 0, -1);
-echo $vidtitle;
 
 $getvideouid = "SELECT ownerid FROM videos WHERE titlehash='$vidtitle';";
 $uidreturned = $sqlconn->query($getvideouid);
-echo "<br>";
 $uidarray = mysqli_fetch_assoc($uidreturned);
 $viduid = $uidarray['ownerid'];
-echo $viduid;
 
 $fullpath = $_SERVER["DOCUMENT_ROOT"]. "/videos/".$viduid."/".$vidtitle;
-echo $fullpath."<br>";
-
 
 $getemail = "SELECT email FROM users WHERE user_id='$viduid';";
 $emailreturned = $sqlconn->query($getemail);
 $emailarray = mysqli_fetch_assoc($emailreturned);
 $videmail = $emailarray['email'];
-echo "<br>";
-echo $videmail;
 
 if($_SESSION['username'] == $videmail){
     $deletevid = "DELETE FROM videos WHERE titlehash='$vidtitle';";
@@ -33,6 +26,4 @@ if($_SESSION['username'] == $videmail){
         echo "File Deleted <meta http-equiv=\"Refresh\" content=\"2; url=/videos.php\">";
     }
 }
-
-
 ?>
--- html/videos.php
+++ html/videos.php
@@ -35,24 +35,6 @@
                 echo "</article>";
             }
         ?>
- 
-	<!--<div class="vcenter text">
-		<div class="vcenterholder center">
-			<h1>All</h1>
-			<article class="video">
-				<video width="320" height="240" controls>
-					<source src="assets/movies/cloth_ball.mp4" type="video/mp4">
-					Your browser does not support the video tag.
-				</video>
-				<p>Uploaded by: USERNAME</p>
-			</article>
-			<article class="video">
-				<video width="320" height="240" controls>
-					<source src="assets/movies/cloth_ball.mp4" type="video/mp4">
-					Your browser does not support the video tag.
-				</video>
-				<p>Uploaded by: USERNAME</p>
-			</article>-->
 		</div>
 	</div>
 	</div>
