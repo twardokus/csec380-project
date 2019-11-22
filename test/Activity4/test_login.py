98ffd5cf02b1fcfbc9b5b1635547328eefde3713
Geoff Twardokus <gdt5762@rit.edu>
2019-11-22 11:57:36 -0500

    formatting changes

--- test/Activity4/test_login.py
+++ test/Activity4/test_login.py
@@ -6,45 +6,49 @@ import time
 
 s = requests.Session()
 
-def login(username, password):
-	url = "http://localhost/loginvalidate.php"	
-	validCreds = {'username': username,'password': password}
-	result = s.post(url, validCreds, allow_redirects=True)
 
-	return result
+def login(username, password):
+    url = "http://localhost/loginvalidate.php"
+    validCreds = {'username': username, 'password': password}
+    result = s.post(url, validCreds, allow_redirects=True)
+
+    return result
+
 
 def test_login():
-	while("mysqli_connect" in login('notarealuser','badpassword').text):
-		time.sleep(1)
-	# Test valid credentials
-	
-	loginResponse = login('admin@rit.edu','password')
+    while "mysqli_connect" in login('notarealuser', 'badpassword').text:
+        time.sleep(1)
+    # Test valid credentials
 
-	assert(loginResponse.status_code == 200)
-	print(loginResponse.text)
-	assert("(Cookies in use)" not in loginResponse.text and '<meta http-equiv="Refresh" content="0" url="http://localhost/login.php" />' not in loginResponse.text)
+    loginResponse = login('admin@rit.edu', 'password')
 
-	# Test bad username and good password
+    assert (loginResponse.status_code == 200)
+    print(loginResponse.text)
+    assert (
+            "(Cookies in use)" not in loginResponse.text and '<meta http-equiv="Refresh" content="0" '
+                                                             'url="http://localhost/login.php" />' not in
+            loginResponse.text)
 
-	
+
+# Test bad username and good password
 
 
 def wait_for_docker_compose():
-	failures = 0
-	while failures < 10:
-		try:
-			home = requests.get("http://localhost", allow_redirects=True)
-			break
-		except:
-			failures += 1
-			time.sleep(30)	
-				
+    failures = 0
+    while failures < 10:
+        try:
+            home = requests.get("http://localhost", allow_redirects=True)
+            break
+        except:
+            failures += 1
+            time.sleep(30)
+
 
 def test_connection():
-	home = s.get("http://127.0.0.1:80", allow_redirects=True)
-	assert(home != None)
+    home = s.get("http://127.0.0.1:80", allow_redirects=True)
+    assert (home != None)
+
 
 wait_for_docker_compose()
 test_connection()
 test_login()
-
