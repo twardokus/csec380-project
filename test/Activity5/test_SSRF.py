ff7072488961a7683c271933eab036dce360ff8b
Andrew Afonso <ara1494@rit.edu>
2019-11-24 15:14:52 -0500

    Create test_SSRF.py

---
+++ test/Activity5/test_SSRF.py
@@ -0,0 +1,59 @@
+# Basic unit test for the Hello World webpage
+
+from bs4 import BeautifulSoup
+import requests
+import time
+
+s = requests.Session()
+
+"""
+Log into the application
+"""
+def login(username, password):
+    url = "http://localhost/loginvalidate.php"
+    validCreds = {'username': username, 'password': password}
+    result = s.post(url, validCreds, allow_redirects=True)
+
+    return result
+
+"""
+Test video upload via file
+"""
+def test_ssrf():
+    login('admin@rit.edu','password')
+    url = 'http://localhost/proc/uploader.php'
+    params = {'vidtitle':'catz'}
+    f=open('html/videos/testvids/438ad3bf24fed937085ffa101ed06cdb23e30007.mp4','rb')
+    result = s.post(url, params, files={'upfile':f})
+    print(result.text)
+    assert(html/videos/1/Array)
+
+
+
+"""
+Utility function to delay test execution until docker-compose has brought all container online.
+"""
+def wait_for_docker_compose():
+    failures = 0
+    while failures < 10:
+        try:
+            home = requests.get("http://localhost", allow_redirects=True)
+            break
+        except:
+            failures += 1
+            time.sleep(30)
+
+"""
+Test whether the web server is online
+"""
+def test_connection():
+    home = s.get("http://127.0.0.1:80", allow_redirects=True)
+    assert (home != None)
+
+"""
+Execute test sequence
+"""
+wait_for_docker_compose()
+test_connection()
+test_ssrf()
+
