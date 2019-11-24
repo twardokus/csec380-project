275bc3ae264cfc1c6ab4bc693c888be11124ab30
Andrew Afonso <ara1494@rit.edu>
2019-11-24 13:27:56 -0500

    Update travistest.php

--- html/travistest.php
+++ html/travistest.php
@@ -1,10 +1,2 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>PHP Info</title>
-		<?php include("proc/head.php"); ?>
-	</head>
-	<body>
+
 	<?php echo get_current_user() ."<br>";echo exec('whoami'); ?>
-	</body>
-</html>
