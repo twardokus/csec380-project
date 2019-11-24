71c68aa51f84950545cfb8f6910b023bad60129d
ejp3496 <43967089+ejp3496@users.noreply.github.com>
2019-11-24 15:45:04 -0500

    Add files via upload

---
+++ test/Activity5/test_sqli.py
@@ -0,0 +1,97 @@
+# Basic unit test for the Hello World webpage
+
+from bs4 import BeautifulSoup
+import requests
+import time
+
+s = requests.Session()
+
+"""
+Log into the application
+"""
+def login(username, password):
+    url = "http://localhost/loginvalidate.php"
+    validCreds = {'username': username, 'password': password}
+    result = s.post(url, validCreds, allow_redirects=True)
+
+    return result
+
+"""
+Test video upload via file
+"""
+def test_uploadvid():
+    login('admin@rit.edu','password')
+    url = 'http://localhost/proc/uploader.php'
+    params = {'vidtitle':'catz'}
+    f=open('html/videos/testvids/438ad3bf24fed937085ffa101ed06cdb23e30007.mp4','rb')
+    result = s.post(url, params, files={'upfile':f})
+
+    print(result.text)
+    assert('File data uploaded to DB sucessfully.' in result.text)
+
+def uploadvid():
+    url = 'http://localhost/proc/uploader.php'
+    titles = ['red fish', 'blue fish', 'Epstein didnt kill himself']
+    
+    for t in titles:
+        params = {'vidtitle':t}
+        f=open('html/videos/testvids/438ad3bf24fed937085ffa101ed06cdb23e30007.mp4','rb')
+        result = s.post(url, params, files={'upfile':f})
+
+        print(result.text)
+        assert('File data uploaded to DB sucessfully.' in result.text)
+
+"""
+Classic SQLi test
+"""
+def test_classicsqli():
+    uploadvid()
+    url = 'http://localhost/videos.php'
+    #params = {'title':"test' or 1=1;--"}
+    params = {'title':"test"}
+    result = s.post(url,params)
+
+    print(result.text)
+    assert('Epstein didnt kill himself' in result.text)
+
+"""
+Test video access
+"""
+def test_accessvid():
+    url = 'http://localhost/videos.php'
+    result = s.get(url)
+
+    soup = BeautifulSoup(result.text, 'lxml')
+    i=soup.find_all('source')[0]['src'].split('/')[-1]+'\\'
+    print(i)
+    assert('Uploaded by: admin@rit.edu' in result.text)
+    return i
+ 
+
+"""
+Utility function to delay test execution until docker-compose has brought all container online.
+"""
+def wait_for_docker_compose():
+    failures = 0
+    while failures < 10:
+        try:
+            home = requests.get("http://localhost", allow_redirects=True)
+            break
+        except:
+            failures += 1
+            time.sleep(30)
+
+"""
+Test whether the web server is online
+"""
+def test_connection():
+    home = s.get("http://127.0.0.1:80", allow_redirects=True)
+    assert (home != None)
+
+"""
+Execute test sequence
+"""
+wait_for_docker_compose()
+test_connection()
+test_uploadvid()
+test_classicsqli()
